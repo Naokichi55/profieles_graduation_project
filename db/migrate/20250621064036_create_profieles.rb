95140f3e6a496a38f7c51aaa6494005cc91d27c9
Naokichi55 <ibosanganba@gmail.com>
2025-06-21 16:43:45 +0900

    add:作成したprofilesテーブルをrails db:migrateでdbに反映

--- db/migrate/20250621064036_create_profieles.rb
+++ db/migrate/20250621064036_create_profieles.rb
@@ -1,6 +1,12 @@
 class CreateProfieles < ActiveRecord::Migration[7.2]
   def change
     create_table :profieles do |t|
+      t.string :racket_name
+      t.string :history
+      t.string :play_style
+      t.string :level
+      t.string :generation
+      t.string :gender 
       t.timestamps
     end
   end
