cdeb60f9d999293b02aa1b1c66c9d286c6cde7ad
Naokichi55 <ibosanganba@gmail.com>
2025-06-21 21:47:34 +0900

    add:投稿機能controllerとviewを作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,1 +1,20 @@
-class ProfilesController
+class ProfilesController < ApplicationController
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+		@profile = current_user.profiles.build(profile_params)
+		if @profile.save
+      redirect_to profile_path 
+		else
+      flash.now[:danger] = "プロフィールの作成に失敗した"
+			render :new, status: :unprocessable_entity
+		end
+  end
+
+private
+	def  profile_params
+		params.require(:profile).permit(:racket, :history, :play_style, :level, :generation, :gender)
+	end
+end
